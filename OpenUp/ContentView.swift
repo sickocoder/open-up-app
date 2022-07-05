791e3b7420be5b1512a28c1e72f9c4ba543f135c
Jose Tone <developer.tony17@gmail.com>
2022-07-05 11:13:50 +0100

    Initial Commit

---
+++ OpenUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpenUp
+//
+//  Created by José Tony on 05/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpenUp/OpenUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenUpApp.swift
+//  OpenUp
+//
+//  Created by José Tony on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
