5c684ca97652a62e9bd3af4965020b4da92c09af
Jose Tone <developer.tony17@gmail.com>
2022-07-27 12:24:54 +0100

    feat: configure firebase

--- OpenUp/OpenUpApp.swift
+++ OpenUp/OpenUpApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    
+    return true
+  }
+}
 
 @main
 struct OpenUpApp: App {
-    var body: some Scene {
-        WindowGroup {
-            OpenUpView()
-        }
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
+  var body: some Scene {
+    WindowGroup {
+      OpenUpView()
     }
+  }
 }
